831ea716707b28456542ece3bfabe511bda59cee
royal2710 <royal2710@outlook.com>
2019-05-23 10:35:31 +0800

    first commit: hello world!

---
+++ App.js
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+
+import LoginScreen from './src/screens/Login/LoginScreen';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+const MainNavigator = createStackNavigator({
+  Login: {
+    screen: LoginScreen,
+    navigationOptions: {
+      headerStyle: { display: 'none' },
+      headerLeft: null
+    }
+  }
+}, { initialRouteName: "Login"});
+
+const AppContainer = createAppContainer(MainNavigator);
+
+export default class App extends Component {
+  render() {
+    return ( <AppContainer /> );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
