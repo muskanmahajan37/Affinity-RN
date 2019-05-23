31e801c089d4396514c39ed3d2c35ff7a4f12edd
softman009 <softman009@outlook.com>
2019-05-23 16:53:33 +0800

    SignAndSend without SignCapture Modal

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ import { createStackNavigator, createAppContainer } from 'react-navigation';
 import LoginScreen from './src/screens/Login/LoginScreen';
 import ControlPanelScreen from './src/screens/ControlPanel/ControlPanelScreen';
 import DailyCareNotesScreen from './src/screens/DailyCareNotes/DailyCareNotesScreen';
+import SignAndSendScreen from './src/screens/SignAndSend/SignAndSendScreen';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -42,8 +43,15 @@ const MainNavigator = createStackNavigator({
       headerStyle: { display: 'none' },
       headerLeft: null
     }
+  },
+  SignAndSend: {
+    screen: SignAndSendScreen,
+    navigationOptions: {
+      headerStyle: { display: 'none' },
+      headerLeft: null
+    }
   }
-}, { initialRouteName: "Login"});
+}, { initialRouteName: "SignAndSend"});
 
 const AppContainer = createAppContainer(MainNavigator);
 
