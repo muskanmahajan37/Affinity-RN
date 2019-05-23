4177093a757926cf68068fb5cccdd4df732a38b9
softman009 <softman009@outlook.com>
2019-05-23 10:35:31 +0800

    first commit: hello world!

---
+++ src/screens/Login/LoginScreen.js
@@ -0,0 +1,20 @@
+
+import React, {Component} from 'react';
+import { Text, View} from 'react-native';
+
+class LoginScreen extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render () {
+        return (
+            <View><Text>Hello LoginScreen</Text></View>
+        )
+    }
+}
+
+export default LoginScreen;
