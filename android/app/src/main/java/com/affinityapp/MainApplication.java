6f84b18d3b1db886473d8c19ffcc20540b21e93d
softman009 <softman009@outlook.com>
2019-06-02 09:45:42 +0800

    DCN screen capture

--- android/app/src/main/java/com/affinityapp/MainApplication.java
+++ android/app/src/main/java/com/affinityapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.affinityapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import fr.greweb.reactnativeviewshot.RNViewShotPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.rssignaturecapture.RSSignatureCapturePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNViewShotPackage(),
             new AsyncStoragePackage(),
             new RSSignatureCapturePackage(),
             new RNGestureHandlerPackage()
