6c81f74a4300b782805df13465352892dd7a248d
softman009 <softman009@outlook.com>
2019-06-08 05:46:31 +0800

    spiner timepicker added to DCNTimePicker

--- src/screens/DailyCareNotes/Components/DCNTimePicker.js
+++ src/screens/DailyCareNotes/Components/DCNTimePicker.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text, TouchableOpacity, Modal, StyleSheet, TouchableHighlight, Picker, Alert } from 'react-native';
 import DatePicker from 'react-native-datepicker';
+import { calcHoursPerDay, convertHour } from '../../../helpers/AFDate';
 
 class DCNTimePicker extends Component {
     constructor(props) {
@@ -41,80 +42,80 @@ class DCNTimePicker extends Component {
         }
     }
     
-    selectTime = (value, index) => {
+    selectTime = (selectedTime) => {
         switch (this.state.timeType) {
             // ------ TimeIn
             case 'timeIn1' : 
-                if( global.TimeOut_1_Arr[this.state.timeIndex] && index >= global.TimeOut_1_Arr[this.state.timeIndex] ) {
+                if( convertHour(global.TimeOut_1_Arr[this.state.timeIndex]) && convertHour(selectedTime) >= convertHour(global.TimeOut_1_Arr[this.state.timeIndex]) ) {
                     Alert.alert('', 'Invalid Input Time! Please select correct time.');
                 } else {
-                    global.TimeIn_1_Arr[this.state.timeIndex] = index;
-                    this.setState({time: value});
+                    global.TimeIn_1_Arr[this.state.timeIndex] = selectedTime;
+                    this.setState({time: selectedTime});
                 }
                 break;
             case 'timeIn2' : 
-                if( global.TimeOut_2_Arr[this.state.timeIndex] && index >= global.TimeOut_2_Arr[this.state.timeIndex] ) {
+                if( convertHour(global.TimeOut_2_Arr[this.state.timeIndex]) && convertHour(selectedTime) >= convertHour(global.TimeOut_2_Arr[this.state.timeIndex]) ) {
                     Alert.alert('', 'Invalid Input Time! Please select correct time.');
                 } else {
-                    global.TimeIn_2_Arr[this.state.timeIndex] = index;
-                    this.setState({time: value});
+                    global.TimeIn_2_Arr[this.state.timeIndex] = selectedTime;
+                    this.setState({time: selectedTime});
                 }
                 break;
             case 'timeIn3' : 
-                if( global.TimeOut_3_Arr[this.state.timeIndex] && index >= global.TimeOut_3_Arr[this.state.timeIndex] ) {
+                if( convertHour(global.TimeOut_3_Arr[this.state.timeIndex]) && convertHour(selectedTime) >= convertHour(global.TimeOut_3_Arr[this.state.timeIndex]) ) {
                     Alert.alert('', 'Invalid Input Time! Please select correct time.');
                 } else {
-                    global.TimeIn_3_Arr[this.state.timeIndex] = index;
-                    this.setState({time: value});
+                    global.TimeIn_3_Arr[this.state.timeIndex] = selectedTime;
+                    this.setState({time: selectedTime});
                 }
                 break;
             case 'timeIn4' : 
-                if( global.TimeOut_4_Arr[this.state.timeIndex] && index >= global.TimeOut_4_Arr[this.state.timeIndex] ) {
+                if( convertHour(global.TimeOut_4_Arr[this.state.timeIndex]) && convertHour(selectedTime) >= convertHour(global.TimeOut_4_Arr[this.state.timeIndex]) ) {
                     Alert.alert('', 'Invalid Input Time! Please select correct time.');
                 } else {
-                    global.TimeIn_4_Arr[this.state.timeIndex] = index;
-                    this.setState({time: value});
+                    global.TimeIn_4_Arr[this.state.timeIndex] = selectedTime;
+                    this.setState({time: selectedTime});
                 }
                 break;
             // ------ TimeOut
             case 'timeOut1' : 
-                if( global.TimeIn_1_Arr[this.state.timeIndex] && index <= global.TimeIn_1_Arr[this.state.timeIndex] ) {
+                if( convertHour(global.TimeIn_1_Arr[this.state.timeIndex]) && convertHour(selectedTime) <= convertHour(global.TimeIn_1_Arr[this.state.timeIndex]) ) {
                     Alert.alert('', 'Invalid Input Time! Please select correct time.');
                 } else {
-                    global.TimeOut_1_Arr[this.state.timeIndex] = index;
-                    this.setState({time: value});
+                    global.TimeOut_1_Arr[this.state.timeIndex] = selectedTime;
+                    this.setState({time: selectedTime});
                 }
                 break;
             case 'timeOut2' : 
-                if( global.TimeIn_2_Arr[this.state.timeIndex] && index <= global.TimeIn_2_Arr[this.state.timeIndex] ) {
+                if( convertHour(global.TimeIn_2_Arr[this.state.timeIndex]) && convertHour(selectedTime) <= convertHour(global.TimeIn_2_Arr[this.state.timeIndex]) ) {
                     Alert.alert('', 'Invalid Input Time! Please select correct time.');
                 } else {
-                    global.TimeOut_2_Arr[this.state.timeIndex] = index;
-                    this.setState({time: value});
+                    global.TimeOut_2_Arr[this.state.timeIndex] = selectedTime;
+                    this.setState({time: selectedTime});
                 }
                 break;
             case 'timeOut3' : 
-                if( global.TimeIn_3_Arr[this.state.timeIndex] && index <= global.TimeIn_3_Arr[this.state.timeIndex] ) {
+                if( convertHour(global.TimeIn_3_Arr[this.state.timeIndex]) && convertHour(selectedTime) <= convertHour(global.TimeIn_3_Arr[this.state.timeIndex]) ) {
                     Alert.alert('', 'Invalid Input Time! Please select correct time.');
                 } else {
-                    global.TimeOut_3_Arr[this.state.timeIndex] = index;
-                    this.setState({time: value});
+                    global.TimeOut_3_Arr[this.state.timeIndex] = selectedTime;
+                    this.setState({time: selectedTime});
                 }
                 break;
             case 'timeOut4' : 
-                if( global.TimeIn_4_Arr[this.state.timeIndex] && index <= global.TimeIn_4_Arr[this.state.timeIndex] ) {
+                if( convertHour(global.TimeIn_4_Arr[this.state.timeIndex]) && convertHour(selectedTime) <= convertHour(global.TimeIn_4_Arr[this.state.timeIndex]) ) {
                     Alert.alert('', 'Invalid Input Time! Please select correct time.');
                 } else {
-                    global.TimeOut_4_Arr[this.state.timeIndex] = index;
-                    this.setState({time: value});
+                    global.TimeOut_4_Arr[this.state.timeIndex] = selectedTime;
+                    this.setState({time: selectedTime});
                 }
                 break;
         }
         
-        var HoursPerDay_1 = (global.TimeIn_1_Arr[this.state.timeIndex] && global.TimeOut_1_Arr[this.state.timeIndex]) ? (global.TimeOut_1_Arr[this.state.timeIndex] - global.TimeIn_1_Arr[this.state.timeIndex]) : 0;
-        var HoursPerDay_2 = (global.TimeIn_2_Arr[this.state.timeIndex] && global.TimeOut_2_Arr[this.state.timeIndex]) ? (global.TimeOut_2_Arr[this.state.timeIndex] - global.TimeIn_2_Arr[this.state.timeIndex]) : 0;
-        var HoursPerDay_3 = (global.TimeIn_3_Arr[this.state.timeIndex] && global.TimeOut_3_Arr[this.state.timeIndex]) ? (global.TimeOut_3_Arr[this.state.timeIndex] - global.TimeIn_3_Arr[this.state.timeIndex]) : 0;
-        var HoursPerDay_4 = (global.TimeIn_4_Arr[this.state.timeIndex] && global.TimeOut_4_Arr[this.state.timeIndex]) ? (global.TimeOut_4_Arr[this.state.timeIndex] - global.TimeIn_4_Arr[this.state.timeIndex]) : 0;
+        var HoursPerDay_1 = (global.TimeIn_1_Arr[this.state.timeIndex] && global.TimeOut_1_Arr[this.state.timeIndex]) ? calcHoursPerDay(global.TimeOut_1_Arr[this.state.timeIndex], global.TimeIn_1_Arr[this.state.timeIndex]) : 0;
+        var HoursPerDay_2 = (global.TimeIn_2_Arr[this.state.timeIndex] && global.TimeOut_2_Arr[this.state.timeIndex]) ? calcHoursPerDay(global.TimeOut_2_Arr[this.state.timeIndex], global.TimeIn_2_Arr[this.state.timeIndex]) : 0;
+        var HoursPerDay_3 = (global.TimeIn_3_Arr[this.state.timeIndex] && global.TimeOut_3_Arr[this.state.timeIndex]) ? calcHoursPerDay(global.TimeOut_3_Arr[this.state.timeIndex], global.TimeIn_3_Arr[this.state.timeIndex]) : 0;
+        var HoursPerDay_4 = (global.TimeIn_4_Arr[this.state.timeIndex] && global.TimeOut_4_Arr[this.state.timeIndex]) ? calcHoursPerDay(global.TimeOut_4_Arr[this.state.timeIndex], global.TimeIn_4_Arr[this.state.timeIndex]) : 0;
         global.HoursPerDay_Arr[this.state.timeIndex] = HoursPerDay_1 + HoursPerDay_2 + HoursPerDay_3 + HoursPerDay_4;
         this.props.selectTime();
     }
@@ -156,7 +157,7 @@ class DCNTimePicker extends Component {
                     }
                     // ... You can check the source to find the other keys.
                     }}
-                    onDateChange={(date) => {this.setState({time: date}); alert(date);}}
+                    onDateChange={(date) => {this.selectTime(date);}}
                 />
             </View>
         );
