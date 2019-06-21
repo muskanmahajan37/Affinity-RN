549754457cb947f409ad2e7d334d3a12c10ef311
ujs <ujs@ujss-Mac.local>
2019-06-21 07:09:44 -0700

    ios - Timepicker cell css fix

--- src/screens/DailyCareNotes/Components/DCNTimePicker.js
+++ src/screens/DailyCareNotes/Components/DCNTimePicker.js
@@ -105,7 +105,7 @@ class DCNTimePicker extends Component {
         return (
             <View style={{}}>
                 <DatePicker
-                    style={{width: 68, height: 35}}
+                    style={{width: '100%', height: 35}}
                     date={this.state.time}
                     mode="time"
                     placeholder=" "
@@ -117,7 +117,7 @@ class DCNTimePicker extends Component {
                     showIcon={false}
                     customStyles={{
                     dateInput: {
-                        width: 68,
+                        width: '100%',
                         height: 35,
                         borderWidth: 0
                     }
